a8e69417f80f895858791dd5e1d5681c1d5f0669
tonyreaves <tonyfreaves@gmail.com>
2018-07-10 23:49:43 -0700

    word is working?

--- letter.js
+++ letter.js
@@ -2,20 +2,25 @@ var inquirer = require('inquirer');
 var input = process.argv[2];
 
 //Letter constructor that word.js will call upon
-function Letter(character) {
-    this.character = character.toUpperCase();
+function Letter(char) {
+    console.log(char)
+    this.character = char;
     this.isGuessed = false;
     //this function checks whether to display true character or blank space
     this.toString = function () {
         if (this.isGuessed == true) {
+            //return?
             console.log(this.character)
         }
+
+        //return?
         else console.log("_")
     }
 
        // this function checks whether character input is correct
         this.isCorrect = function () {
-            if (this.input === character) {
+            if (this.input === this.character) {
+                //check this line too
                 this.isGuessed = true
             }
         }
--- word.js
+++ word.js
@@ -3,25 +3,55 @@ var Letter = require("./letter.js");
 
 function Word(word) {
     this.word = word;
-    this.letterArray = [];
+    this.letters = [];
     this.underscoreArray = [];
+    this.letterArray = [];
+    this.strArray = [];
     this.wordSplitter = function () {
         for (var i = 0; i<this.word.length; i++) {
             this.letterArray[i] = "_";
         }
     }
-    this.showTheLetters = function() {
-		for (var j = 0; j < this.letterArray.length; j++){
-			this.letterArray[j] = new Letter (this.letterArray[j]);
-			this.letterArray[j].toString();
-		}
-	}
+
+    this.showTheLetters = function(){
+        var strArray = this.word.split();  
+        console.log(strArray)
+        for (var i = 0; i<this.strArray.length; i++)
+        {
+            this.letters.push( new Letter(strArray[i]) );
+        }
+    
+    }
+   
+
+    // this.showTheLetters = function() {
+	// 	for (var j = 0; j < this.letterArray.length; j++){
+	// 		this.letterArray[j] = new Letter (this.letterArray[j]);
+	// 		this.letterArray[j].toString();
+	// 	}
+	// }
 }
 
 
-var wordTest = new Word ("punch");
+//need function to display word{,
+//loop thru this.letters
+//for each:
+//call the letter's display();
+//add to display string
+
+//return display string
+//}
+
+//function: make a guess(letter){
+    //return true or false, change remaining guesses if false
+//}
+
+var wordTest = new Word("punch");
+console.log(wordTest.word)
 // letterArray.isCorrect();
+console.log(wordTest.letterArray)
 wordTest.wordSplitter();
+console.log(wordTest.letterArray)
 wordTest.showTheLetters();
 
 // Export for Word constructor
